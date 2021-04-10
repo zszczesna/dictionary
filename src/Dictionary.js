215adad9f18c6d5c42db939fb389054e2131a119
zszczesna <szczesna.zuzia@gmail.com>
2021-04-11 01:32:04 +0200

    Rendered the word meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,18 +1,19 @@
 import React, {useState} from "react"
 import axios from "axios";
-
+import Results from "./Results.js"
 
 export default function Dictionary(){
     let[keyword, setKeword] = useState(" ");
+    let[definition, setDefinition] = useState(null);
     
     function handleSearch(response){
-        console.log(response.data[0]);
+        setDefinition(response.data[0]);
     }
     
     function search(event){
         event.preventDefault();
         //documentation https://dictionaryapi.dev/
-        let apiUrl =  `https://api.dictionaryapi.dev/api/v2/entries/en_UK/${keyword}`;
+        let apiUrl =  `https://api.dictionaryapi.dev/api/v2/entries/en_GB/${keyword}`;
         axios.get(apiUrl).then(handleSearch);
     }
 
@@ -26,6 +27,7 @@ export default function Dictionary(){
             <form onSubmit={search}>
                 <input type="search" autoFocus={true} onChange={handleKeywordChange}/>
             </form>
+            <Results results={definition} />
         </div>
     )
 }
