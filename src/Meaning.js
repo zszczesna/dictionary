714dbc4a54bc419a7ee2678b9106411511d2b1e1
zszczesna <szczesna.zuzia@gmail.com>
2021-04-11 01:54:45 +0200

    added synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,5 +1,5 @@
 import React from "react"
-
+import Synonyms from "./Synonyms.js";
 export default function Meaning(props){
     return <div className="Meaning">
         <h3>{props.meaning.partOfSpeech}</h3>
@@ -8,6 +8,7 @@ export default function Meaning(props){
            <div key={index}>
        <p>{definition.definition}</p>
         <p>{definition.example}</p>
+        <Synonyms synonyms={definition.synonyms}/>
         </div>
        );
         
